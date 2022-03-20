0eb545590a7d474b774de61c887b69d4389a8787
Jeff Katz <git@kraln.com>
2022-03-20 15:48:08 +0100

    Add German Localization
    🇩🇪Localization for the app added. For the impressum, , I'm not going
    to even guess as to what would actually be needed in your case, please
    contact the relevant lawyer.

---
+++ locale-de.js
@@ -0,0 +1,62 @@
+ /*
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * 
+  * You may obtain a copy of the License at
+  * http://www.apache.org/licenses/LICENSE-2.0
+  * 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License
+  *
+  * 
+  *
+  *
+  * german localization
+  * 
+  */
+
+
+(function () {
+  'use strict';
+  window.locale = {
+    link_competition_list: "Wettkämpfe",
+    link_live: "Echtzeit",
+    link_impressum: "Impressum und Datenschutz",
+    link_back_to_competition: "Züruck zum Wettkämpfe",
+    search_placeholder: "Suche...",
+    search_button: "Suche",
+    header_date: "Datum",
+    header_comp_name: "Wettkampf",
+    header_discipline: "Disziplin",
+    header_disciplines: "Disziplin",
+    header_club: "Verein",
+    header_place: "Platz",
+    header_competitor: "Teilnehmer",
+    header_series: "Serien",
+    header_result: "Ergebnisse",
+    header_lane_nr: "Standnummer",
+    header_start_nr: "Startnummer",
+    header_class: "Klasse",
+    header_total: "Gesamt",
+    header_avg_time_per_shot: "Durchschnittszeit",
+    header_best_teiler: "Beste Schüsse",
+    header_worst_teiler: "Schlimmste Schüsse",
+    header_best_s_teiler: "Bester Schuss",
+    header_worst_s_teiler: "Schlimmster Schuss",
+    header_centerpoint: "Trefferlage",
+    header_spread: "Streuwert",
+    header_distribution: "Treffer",
+    header_competition_time: "Wettkampfzeit",
+    header_inner_rings: "Innenzehner",
+    unit_seconds_short: "sek",
+    unit_seconds: "sekunden",
+    unit_mm: "mm",
+    unit_right: "rechts",
+    unit_left: "links",
+    unit_up: "hoch",
+    unit_down: "tief",
+  };
+})();
